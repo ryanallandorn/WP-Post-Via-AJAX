aadcf3e605579e0bd7c18dcf155edef658bba5fd
Ryan Dorn <ryan.allan.dorn@gmail.com>
2017-06-23 10:04:51 -0300

    misc

--- post_via_ajax.php
+++ post_via_ajax.php
@@ -1,11 +1,11 @@
 <?php
-	
+
 /**
- Plugin Name: Post via Ajax
+* Plugin Name: Post via Ajax
  */
- 
+
 /* Enqueue JS
------ */ 
+----- */
 
 function pva_scripts() {
 	wp_register_script( 'pva-js', plugin_dir_url( __FILE__ ) . 'pva.js', array( 'jquery' ), '', true );
@@ -18,16 +18,16 @@ add_action('wp_enqueue_scripts', 'pva_scripts');
 // creating Ajax call for WordPress
 add_action( 'wp_ajax_nopriv_pva_create', 'pva_create' );
 add_action( 'wp_ajax_pva_create', 'pva_create' );
- 
+
 /* WP Insert Post Function
------ */ 
- 
+----- */
+
 function pva_create()
 {
 	$results = '';
 
 	$post_title = $_POST['post_title'];
-	
+
 	// Create post object
 	$new_pva_post = array(
 		'post_type'		=> 'page',
@@ -35,22 +35,22 @@ function pva_create()
 		'post_status'	=> 'publish',
 		'post_author'	=> 1,
 	);
-	 
+
 	// Insert the post into the database
 	wp_insert_post( $new_pva_post );
 	die($results);
 };
 
 
- 
+
 /* Form Shortcode
------ */ 
+----- */
 
 function pva_shortcode( $atts, $content = null ) {
 	ob_start();
 	include(plugin_dir_path( __FILE__ ) . 'post_via_ajax_field.php');
-	$ret = ob_get_contents(); 
-	ob_end_clean(); 
+	$ret = ob_get_contents();
+	ob_end_clean();
 	return $ret;
 	//pva();
 };
